e755bfc4dca1574bb961bc203d82d6d2231e61dd
lff1016 <1037395462@qq.com>
2021-12-14 22:31:53 +0800

    v1.0.2

--- models/connect.js
+++ models/connect.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const config = require('config')
 
-console.log(mongoose)
 mongoose.connect('mongodb://162.14.76.95:27017/w_blog')
     .then(() => console.log('数据库连接成功'))
     .catch(err => console.log(err))
